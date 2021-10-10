cb17dfd8c1fcc55e6e5f4076ad30242ed5370a49
raghvisaxena <201951119@iiitvadodara.ac.in>
2021-10-10 15:18:42 +0530

    added gameover features

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { cloneDeep } from 'lodash';
-
-import { useEvent , getColors} from './util';
+import cloneDeep from "lodash.clonedeep";
+import { useEvent, getColors } from "./util";
+import Swipe from "react-easy-swipe";
 
 function App() {
   const UP_ARROW = 38;
@@ -18,20 +18,25 @@ function App() {
 
   const [gameOver, setGameOver] = useState(false);
 
-  // init - create grid
+  // Initialize
   const initialize = () => {
+    // console.log("CALLING INITIALIZE");
+
     let newGrid = cloneDeep(data);
+    console.log(newGrid);
 
     addNumber(newGrid);
+    console.table(newGrid);
     addNumber(newGrid);
+    console.table(newGrid);
     setData(newGrid);
   };
 
-  // addNumber - add an item
+  // AddNumber - Add an item
   const addNumber = (newGrid) => {
     let added = false;
     let gridFull = false;
-
+    let attempts = 0;
     while (!added) {
       if (gridFull) {
         break;
@@ -39,16 +44,25 @@ function App() {
 
       let rand1 = Math.floor(Math.random() * 4);
       let rand2 = Math.floor(Math.random() * 4);
-
+      attempts++;
       if (newGrid[rand1][rand2] === 0) {
-        newGrid[rand1][rand2] = Math.random() > .5 ? 4 : 2;
+        newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
         added = true;
       }
+      if (attempts > 50) {
+        gridFull = true;
+        let gameOverr = checkIfGameOver();
+        if (gameOverr) {
+          alert("game over");
+          // setGameOver(true);
+        }
+        // setGameOver(true);
+      }
     }
-  }
-
-  // swipe left
-  const swipeLeft = (validDir) => {
+  };
+  // Swipe Left
+  const swipeLeft = (dummy) => {
+    console.log("swipe left");
     let oldGrid = data;
     let newArray = cloneDeep(data);
 
@@ -59,7 +73,7 @@ function App() {
       while (slow < 4) {
         if (fast === 4) {
           fast = slow + 1;
-          slow = slow + 1;
+          slow++;
           continue;
         }
         if (b[slow] === 0 && b[fast] === 0) {
@@ -75,9 +89,9 @@ function App() {
             b[slow] = b[slow] + b[fast];
             b[fast] = 0;
             fast = slow + 1;
-            slow = slow + 1;
+            slow++;
           } else {
-            slow = slow + 1;
+            slow++;
             fast = slow + 1;
           }
         }
@@ -86,14 +100,15 @@ function App() {
     if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
       addNumber(newArray);
     }
-    if (validDir) {
+    if (dummy) {
       return newArray;
     } else {
       setData(newArray);
     }
   };
 
-  const swipeRight = (validDir) => {
+  const swipeRight = (dummy) => {
+    console.log("swipe right");
     let oldData = data;
     let newArray = cloneDeep(data);
 
@@ -131,14 +146,16 @@ function App() {
     if (JSON.stringify(newArray) !== JSON.stringify(oldData)) {
       addNumber(newArray);
     }
-    if (validDir) {
+    if (dummy) {
       return newArray;
     } else {
       setData(newArray);
     }
   };
 
-  const swipeDown = (validDir) => {
+  const swipeDown = (dummy) => {
+    console.log("swipe down");
+    console.log(data);
     let b = cloneDeep(data);
     let oldData = JSON.parse(JSON.stringify(data));
     for (let i = 3; i >= 0; i--) {
@@ -174,14 +191,15 @@ function App() {
     if (JSON.stringify(b) !== JSON.stringify(oldData)) {
       addNumber(b);
     }
-    if (validDir) {
+    if (dummy) {
       return b;
     } else {
       setData(b);
     }
   };
 
-  const swipeUp = (validDir) => {
+  const swipeUp = (dummy) => {
+    console.log("swipe up");
     let b = cloneDeep(data);
     let oldData = JSON.parse(JSON.stringify(data));
     for (let i = 0; i < 4; i++) {
@@ -190,7 +208,7 @@ function App() {
       while (slow < 4) {
         if (fast === 4) {
           fast = slow + 1;
-          slow = slow + 1;
+          slow++;
           continue;
         }
         if (b[slow][i] === 0 && b[fast][i] === 0) {
@@ -206,9 +224,9 @@ function App() {
             b[slow][i] = b[slow][i] + b[fast][i];
             b[fast][i] = 0;
             fast = slow + 1;
-            slow = slow + 1;
+            slow++;
           } else {
-            slow = slow + 1;
+            slow++;
             fast = slow + 1;
           }
         }
@@ -217,92 +235,203 @@ function App() {
     if (JSON.stringify(oldData) !== JSON.stringify(b)) {
       addNumber(b);
     }
-    if (validDir) {
+    if (dummy) {
       return b;
     } else {
       setData(b);
     }
   };
 
-  // Check if gameover
+  // Check Gameover
   const checkIfGameOver = () => {
-    let leftCheck = swipeLeft(true);
-    if (JSON.stringify(data) !== JSON.stringify(leftCheck)) {
+    console.log("CHECKING GAME OVER");
+    // let original = cloneDeep(data);
+    let checker = swipeLeft(true);
+
+    if (JSON.stringify(data) !== JSON.stringify(checker)) {
       return false;
     }
 
-    let rightCheck = swipeRight(true);
-    if (JSON.stringify(data) !== JSON.stringify(rightCheck)) {
+    let checker2 = swipeDown(true);
+    console.log("CHECKER DOWN");
+    console.table(data);
+    console.table(checker2);
+    if (JSON.stringify(data) !== JSON.stringify(checker2)) {
       return false;
     }
 
-    let downCheck = swipeDown(true);
-    if (JSON.stringify(data) !== JSON.stringify(downCheck)) {
+    let checker3 = swipeRight(true);
+
+    if (JSON.stringify(data) !== JSON.stringify(checker3)) {
       return false;
     }
 
-    let upCheck = swipeUp(true);
-    if (JSON.stringify(data) !== JSON.stringify(upCheck)) {
+    let checker4 = swipeUp(true);
+
+    if (JSON.stringify(data) !== JSON.stringify(checker4)) {
       return false;
     }
 
     return true;
+  };
+  // Reset
+  const resetGame = () => {
+    setGameOver(false);
+    const emptyGrid = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
+    addNumber(emptyGrid);
+    addNumber(emptyGrid);
+    setData(emptyGrid);
   };
 
   const handleKeyDown = (event) => {
     if (gameOver) {
       return;
     }
-
     switch (event.keyCode) {
       case UP_ARROW:
+        // alert("up");
+        // console.table(data);
         swipeUp();
+        // console.table(data);
         break;
       case DOWN_ARROW:
+        // console.table(data);
         swipeDown();
+        // console.table(data);
         break;
       case LEFT_ARROW:
+        // console.table(data);
         swipeLeft();
+        // console.table(data);
         break;
       case RIGHT_ARROW:
+        // console.table(data);
         swipeRight();
+        // console.table(data);
         break;
       default:
         break;
     }
 
-    let isGameOver = checkIfGameOver();
-    if (isGameOver) {
+    let gameOverr = checkIfGameOver();
+    if (gameOverr) {
       setGameOver(true);
     }
   };
 
   useEffect(() => {
     initialize();
+    // eslint-disable-next-line
   }, []);
 
+  // This is a custom function
   useEvent("keydown", handleKeyDown);
 
   return (
-    <div
-      style={{
-        background: "#AD9D8F",
-        width: "max-content",
-        margin: "auto",
-        padding: 5,
-        borderRadius: 5,
-        marginTop: 10,
-      }}
-    >
-      {data.map((row, oneIndex) => {
-        return (
-          <div style={{ display: "flex" }} key={oneIndex}>
-            {row.map((digit, index) => (
-              <Block num={digit} key={index} />
-            ))}
+    <div className="App">
+      <div
+        style={{
+          width: 345,
+          margin: "auto",
+          marginTop: 30,
+        }}
+      >
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              fontFamily: "sans-serif",
+              flex: 1,
+              fontWeight: "700",
+              fontSize: 60,
+              color: "#776e65",
+            }}
+          >
+            2048
           </div>
-        );
-      })}
+          <div
+            style={{
+              flex: 1,
+              marginTop: "auto",
+            }}
+          >
+            <div onClick={resetGame} style={style.newGameButton}>
+              NEW GAME
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            background: "#AD9D8F",
+            width: "max-content",
+            height: "max-content",
+            margin: "auto",
+            padding: 5,
+            borderRadius: 5,
+            marginTop: 10,
+            position: "relative",
+          }}
+        >
+          {gameOver && (
+            <div style={style.gameOverOverlay}>
+              <div>
+                <div
+                  style={{
+                    fontSize: 30,
+                    fontFamily: "sans-serif",
+                    fontWeight: "900",
+                    color: "#776E65",
+                  }}
+                >
+                  Game Over
+                </div>
+                <div>
+                  <div
+                    style={{
+                      flex: 1,
+                      marginTop: "auto",
+                    }}
+                  >
+                    <div onClick={resetGame} style={style.tryAgainButton}>
+                      Try Again
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+          <Swipe
+            onSwipeDown={() => {
+              swipeDown();
+            }}
+            onSwipeLeft={() => swipeLeft()}
+            onSwipeRight={() => swipeRight()}
+            onSwipeUp={() => swipeUp()}
+            style={{ overflowY: "hidden" }}
+          >
+            {data.map((row, oneIndex) => {
+              return (
+                <div style={{ display: "flex" }} key={oneIndex}>
+                  {row.map((digit, index) => (
+                    <Block num={digit} key={index} />
+                  ))}
+                </div>
+              );
+            })}
+          </Swipe>
+        </div>
+
+        <div style={{ width: "inherit" }}>
+          <p class="gameExplanation" style={{margin:5}}>
+            Use your{" "}<strong>arrow keys</strong> to move the tiles. Enjoy!!!
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
@@ -310,15 +439,15 @@ function App() {
 const Block = ({ num }) => {
   const { blockStyle } = style;
 
-  return ( 
+  return (
     <div
       style={{
         ...blockStyle,
-         background: getColors(num),
+        background: getColors(num),
         color: num === 2 || num === 4 ? "#645B52" : "#F7F4EF",
       }}
     >
-      {num !== 0 && num}
+      {num !== 0 ? num : ""}
     </div>
   );
 };
@@ -336,5 +465,40 @@ const style = {
     fontWeight: "800",
     color: "white",
   },
-}
+  newGameButton: {
+    padding: 10,
+    background: "#846F5B",
+    color: "#F8F5F0",
+    width: 95,
+    borderRadius: 7,
+    fontWeight: "900",
+    marginLeft: "auto",
+    marginBottom: "auto",
+    cursor: "pointer",
+  },
+  tryAgainButton: {
+    padding: 10,
+    background: "#846F5B",
+    color: "#F8F5F0",
+    width: 80,
+    borderRadius: 7,
+    fontWeight: "900",
+    cursor: "pointer",
+    margin: "auto",
+    marginTop: 20,
+  },
+  gameOverOverlay: {
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    left: 0,
+    top: 0,
+    borderRadius: 5,
+    background: "rgba(238,228,218,.5)",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
+
 export default App;
